62b512d02362f957469994d01ab0099de131d608
7we <peterhenryd@gmail.com>
2020-06-26 20:52:40 -0400

    GUI Updates

--- src/main/resources/test.kt
+++ src/main/resources/test.kt
@@ -4,6 +4,8 @@ import xyz.monology.spigotlin.KotlinPlugin
 import xyz.monology.spigotlin.dsl.event
 import xyz.monology.spigotlin.dsl.gui
 import xyz.monology.spigotlin.dsl.item
+import xyz.monology.spigotlin.dsl.rowRange
+import xyz.monology.spigotlin.utils.colored
 import xyz.monology.spigotlin.utils.openGui
 
 class MyPlugin : KotlinPlugin() {
@@ -11,7 +13,15 @@ class MyPlugin : KotlinPlugin() {
         event<PlayerJoinEvent> {
             player.openGui(gui("Welcome!", 3) {
                 // GUI slots start at 1, not 0!
-                this[2, 5] = item("Leave", Material.BARRIER)
+                this[2, 5] = item("Leave", Material.BARRIER).onClick {
+                    player.kickPlayer("&bWell, you asked to leave!".colored)
+                }
+
+                fill(item("&8Please select a button.", Material.GRAY_STAINED_GLASS_PANE))
+
+                this[rowRange(3, 1, 9)] = item("Close the menu.").onClick {
+                    player.closeInventory()
+                }
             })
         }
     }
